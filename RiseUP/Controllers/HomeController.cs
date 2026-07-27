1cf133d4a144119a53f63fd249feaab097603091
Md.Srabon <mdsrabon.cse@gmail.com>
2026-07-27 23:23:12 +0600

    home controller

--- RiseUP/Controllers/HomeController.cs
+++ RiseUP/Controllers/HomeController.cs
@@ -1,25 +1,42 @@
 using Microsoft.AspNetCore.Mvc;
-using RiseUP.Models;
-using System.Diagnostics;
+using Microsoft.EntityFrameworkCore;
+using RiseUp.Data;
+using RiseUp.Models;
 
-namespace RiseUP.Controllers
+namespace RiseUp.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        private readonly ApplicationDbContext _context;
+
+        public HomeController(ApplicationDbContext context)
         {
-            return View();
+            _context = context;
         }
 
-        public IActionResult Privacy()
+        public async Task<IActionResult> Index(string category = "All")
         {
-            return View();
-        }
+            // Query base for active ideas
+            var ideasQuery = _context.StartupIdeas.Include(i => i.Founder).AsQueryable();
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            if (!string.IsNullOrEmpty(category) && category != "All")
+            {
+                ideasQuery = ideasQuery.Where(i => i.Category == category);
+            }
+
+            // Populate dashboard data
+            var model = new LandingDashboardViewModel
+            {
+                FeaturedIdeas = await ideasQuery.Where(i => i.IsFeatured).OrderByDescending(i => i.CreatedAt).Take(3).ToListAsync(),
+                RecentIdeas = await ideasQuery.OrderByDescending(i => i.CreatedAt).Take(6).ToListAsync(),
+                TotalIdeasCount = await _context.StartupIdeas.CountAsync(),
+                TotalFoundersCount = await _context.Users.CountAsync(u => u.RoleType == UserType.Founder),
+                TotalInvestorsCount = await _context.Users.CountAsync(u => u.RoleType == UserType.Investor),
+                TotalMentorsCount = await _context.Users.CountAsync(u => u.RoleType == UserType.Mentor),
+                SelectedCategory = category
+            };
+
+            return View(model);
         }
     }
 }
