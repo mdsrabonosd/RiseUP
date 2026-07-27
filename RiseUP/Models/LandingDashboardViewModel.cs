637b0f08eb7e60680f730e8c8ad3a595c7d11897
Md.Srabon <mdsrabon.cse@gmail.com>
2026-07-27 23:37:02 +0600

    dashboard landing page VM

---
+++ RiseUP/Models/LandingDashboardViewModel.cs
@@ -0,0 +1,18 @@
+﻿namespace RiseUp.Models
+{
+    public class LandingDashboardViewModel
+    {
+        // Lists to showcase on the dashboard
+        public IEnumerable<StartupIdea> FeaturedIdeas { get; set; } = new List<StartupIdea>();
+        public IEnumerable<StartupIdea> RecentIdeas { get; set; } = new List<StartupIdea>();
+
+        // Platform Summary Stats
+        public int TotalIdeasCount { get; set; }
+        public int TotalFoundersCount { get; set; }
+        public int TotalInvestorsCount { get; set; }
+        public int TotalMentorsCount { get; set; }
+
+        // Selected category filter
+        public string SelectedCategory { get; set; } = "All";
+    }
+}
